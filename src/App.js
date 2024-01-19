0576fb90043ab336cfba51d131d887594a1da4cb
Sergio Eduardo Frioe <sergio.friou@academy.compass.uol>
2024-01-19 13:35:04 +0200

    added cart visibility state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
+import { useState } from 'react';
+
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+
+ const [cartIsShow,setCartIsShown]=useState(false);
+
+ function showCarthandler() {
+  setCartIsShown(true);
+ 
+
+ }
+
+ function hideCartHandler(){
+  setCartIsShown(false);
+ }
+
   return (
     <CartProvider>
-      <Header />
+      {cartIsShow && <div>Cart....</div>}
+      <Header onShowCart={showCarthandler} />
       <main>
         <Meals />
       </main>
     </CartProvider>
   );
+
 }
 
 export default App;
